b24554f11448d3f86266ca5b864ed862fb9b81f6
Marco Antonio Nakasone Gomes <marcoanakasone@gmail.com>
2025-10-07 18:30:26 -0500

    feat: update App.vue to include OrganizerHeader and router-view for improved layout

--- src/router.js
+++ src/router.js
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
     { path: '/', component: { template: '<h1>Home</h1>' } },
-    { path: '/org/dashboard', component: { template: '<h1>Dashboard Organizador</h1>' } }
+    { path: '/dashboard', component: { template: '<h1>Dashboard Organizador</h1>' } },
+    {
+        path: '/create-event',
+        name: 'create-event',
+        component: () => import('./organizer/pages/create-event.vue')
+    },
+    {
+        path: '/events',
+        name: 'events',
+        component: () => import('./organizer/pages/events.vue')
+    }
+
 ]
 
 const router = createRouter({
